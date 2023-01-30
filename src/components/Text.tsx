a7c32b56fe7a041adf7c6b3ad5e36b3d95195a40
Daft <daftdevs@gmail.com>
2023-01-30 20:46:11 +0700

    feat: add color, bgColor, and variants customization

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,15 +1,23 @@
 import { View, Text as RNText } from "react-native";
 import React from "react";
 import { SpacingProps } from "../types";
-import Theme from "../styles/theme";
+import Theme from "../styles/Theme";
 
 type TextProps = SpacingProps &
   React.ComponentProps<typeof RNText> & {
     color?: string;
     bgColor?: string;
     fontSize?: string;
+    variants?: "h1" | "h2" | "h3" | "h4" | "h5" | "default";
     children: React.ReactNode;
   };
+
+type TextVariantsProps = {
+  [key: string]: {
+    fontSize: number;
+    lineHeight?: number;
+  };
+};
 
 const Text = ({
   p = "none",
@@ -26,13 +34,43 @@ const Text = ({
   mr = "none",
   mb = "none",
   ml = "none",
+  color = "black",
+  bgColor = "transparent",
+  variants = "default",
   ...props
 }: TextProps) => {
+  const textVariants: TextVariantsProps = {
+    h1: {
+      fontSize: 48,
+      lineHeight: 1,
+    },
+    h2: {
+      fontSize: 36,
+      lineHeight: 1,
+    },
+    h3: {
+      fontSize: 30,
+      lineHeight: 1,
+    },
+    h4: {
+      fontSize: 24,
+      lineHeight: 1,
+    },
+    h5: {
+      fontSize: 20,
+      lineHeight: 1,
+    },
+    default: {
+      fontSize: 14,
+    },
+  };
+
   return (
     <View>
       <RNText
         style={[
           props.style,
+          // Spacing Styling
           {
             padding: Theme.spacing[p],
             paddingHorizontal: Theme.spacing[px],
@@ -48,6 +86,12 @@ const Text = ({
             marginRight: Theme.spacing[mr],
             marginBottom: Theme.spacing[mb],
             marginLeft: Theme.spacing[ml],
+          },
+          // Text Styling
+          {
+            color: color,
+            backgroundColor: bgColor,
+            ...textVariants[variants],
           },
         ]}
       >
