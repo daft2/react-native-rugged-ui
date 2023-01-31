6044c2df78ed71c0e7f1b1b7809b642cf3d7a3cd
Daft <daftdevs@gmail.com>
2023-01-31 20:23:29 +0700

    feat: display typography

--- src/screens/TabOneScreen.tsx
+++ src/screens/TabOneScreen.tsx
@@ -1,38 +1,51 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../../types';
+import { StyleSheet } from "react-native";
+import { View } from "../components/Themed";
+import { RootTabScreenProps } from "../../types";
+import Text from "../components/Text";
 
 export default function TabOneScreen({
-    navigation,
-}: RootTabScreenProps<'TabOne'>) {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tab One</Text>
-            <View
-                style={styles.separator}
-                lightColor="#eee"
-                darkColor="rgba(255,255,255,0.1)"
-            />
-            <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-        </View>
-    );
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  return (
+    <View style={styles.container}>
+      <Text variants="h4" mb="sm">
+        Heading
+      </Text>
+      <Text variants="h1" weight="bold">
+        Heading 1
+      </Text>
+      <Text variants="h2">Heading 2</Text>
+      <Text variants="h3">Heading 3</Text>
+      <Text variants="h4">Heading 4</Text>
+      <Text variants="h5">Heading 5</Text>
+      <Text variants="h4" py="sm">
+        Subtitle
+      </Text>
+      <Text variants="s1">Subtitle text 1</Text>
+      <Text variants="s2">Subtitle text 2</Text>
+      <Text variants="h4" py="sm">
+        Paragraph
+      </Text>
+      <Text variants="p">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        beatae eius sint nisi molestiae maiores nihil deleniti dolor. Nihil,
+        vitae.
+      </Text>
+      <Text variants="h4" py="sm">
+        Regular
+      </Text>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        beatae eius sint nisi molestiae maiores nihil deleniti dolor. Nihil,
+        vitae.
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
+  container: {
+    flex: 1,
+    padding: 10,
+  },
 });
