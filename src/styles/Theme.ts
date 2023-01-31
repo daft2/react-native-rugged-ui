8a879d41ecbba5145f414e003e00338faf1975c3
Daft <daftdevs@gmail.com>
2023-01-31 20:17:12 +0700

    ref: make none value to undefined

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,16 +1,16 @@
 const Theme = {
-    spacing: {
-        none: 0,
-        xs: 4,
-        sm: 8,
-        md: 12,
-        lg: 20,
-        xl: 28,
-        '2xl': 38,
-        '3xl': 48,
-        '4xl': 50,
-        '5xl': 62,
-    },
+  spacing: {
+    none: undefined,
+    xs: 4,
+    sm: 8,
+    md: 12,
+    lg: 20,
+    xl: 28,
+    "2xl": 38,
+    "3xl": 48,
+    "4xl": 50,
+    "5xl": 62,
+  },
 };
 
 export default Theme;
