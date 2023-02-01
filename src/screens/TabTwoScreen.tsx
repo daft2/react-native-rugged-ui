9d0792d3ce1f0d075a25b44cfaea9f3220074533
Daft <daftdevs@gmail.com>
2023-02-01 21:25:51 +0700

    feat: showcase forms components screen

--- src/screens/TabTwoScreen.tsx
+++ src/screens/TabTwoScreen.tsx
@@ -1,35 +1,24 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Button as RNButton, StyleSheet, View } from "react-native";
+import Button from "../components/Button";
+import Text from "../components/Text";
 
 export default function TabTwoScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tab Two</Text>
-            <View
-                style={styles.separator}
-                lightColor="#eee"
-                darkColor="rgba(255,255,255,0.1)"
-            />
-            <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text variants="h4" mb="sm">
+        Button
+      </Text>
+      <View style={{ width: 100 }}>
+        <RNButton title="test" color={"red"} />
+      </View>
+      <Button title="test" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
+  container: {
+    flex: 1,
+    padding: 10
+  }
 });
