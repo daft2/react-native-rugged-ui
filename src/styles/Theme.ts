a1b08a31c2615d9bf0e50be6bdcd98e5675a262b
Daft <daftdevs@gmail.com>
2023-02-03 21:28:57 +0700

    feat: add colors theme

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -9,8 +9,31 @@ const Theme = {
     "2xl": 38,
     "3xl": 48,
     "4xl": 50,
-    "5xl": 62,
+    "5xl": 62
   },
+  colors: {
+    // pastel
+    red: "#ffadad",
+    orange: "#ffd6a5",
+    yellow: "#fdffb6",
+    green: "#caffbf",
+    blue: "#9bf6ff",
+    violet: "#a0c4ff",
+    purple: "#bdb2ff",
+    pink: "#ffc6ff",
+    // monochrome
+    white: "#ffffff",
+    black: "#000000",
+    dark: "#25283d",
+    neutral: "#fcfcfc",
+    stone: "#fff9ec",
+    charcoal: "#2e4057",
+    // system
+    info: "#083d77",
+    danger: "#ff3c38",
+    warning: "#F4D35E",
+    success: "#64F58D"
+  }
 };
 
 export default Theme;
