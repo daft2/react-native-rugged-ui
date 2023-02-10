c97a978a747420203dbee9ee8eca9c1825db90b6
Daft <daftdevs@gmail.com>
2023-02-10 22:06:23 +0700

    ref: update typography variants view

--- src/screens/TypographyScreen.tsx
+++ src/screens/TypographyScreen.tsx
@@ -17,23 +17,26 @@ export default function TypographyScreen({
       <Text variants="h3">Heading 3</Text>
       <Text variants="h4">Heading 4</Text>
       <Text variants="h5">Heading 5</Text>
-      <Text variants="h4" py="sm">
+      <Text variants="h6">Heading 6</Text>
+      <Text variants="h4" mt="lg" mb="sm">
         Subtitle
       </Text>
-      <Text variants="s1">Subtitle text 1</Text>
-      <Text variants="s2">Subtitle text 2</Text>
-      <Text variants="h4" py="sm">
+      <Text variants="subtitle1">Subtitle text 1</Text>
+      <Text variants="subtitle2">Subtitle text 2</Text>
+      <Text variants="h4" mt="lg" mb="sm">
         Paragraph
       </Text>
-      <Text variants="p">
+      <Text variants="paragraph1">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
         beatae eius sint nisi molestiae maiores nihil deleniti dolor. Nihil,
         vitae.
       </Text>
-      <Text variants="h4" py="sm">
-        Regular
+      <Text variants="paragraph2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        beatae eius sint nisi molestiae maiores nihil deleniti dolor. Nihil,
+        vitae.
       </Text>
-      <Text>
+      <Text variants="paragraph3">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
         beatae eius sint nisi molestiae maiores nihil deleniti dolor. Nihil,
         vitae.
