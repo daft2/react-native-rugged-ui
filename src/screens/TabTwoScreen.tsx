069ba8f43cb49ea57b7f156991d1ecf5f11dcbfb
Daft <daftdevs@gmail.com>
2023-02-17 22:45:45 +0700

    feat: display test component screen

--- src/screens/TabTwoScreen.tsx
+++ src/screens/TabTwoScreen.tsx
@@ -1,6 +1,8 @@
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 import { Button as RNButton, StyleSheet, View } from "react-native";
 import Button from "../components/Button";
+import Card from "../components/Card";
 import Text from "../components/Text";
 
 export default function TabTwoScreen() {
@@ -12,7 +14,55 @@ export default function TabTwoScreen() {
       <View style={{ width: 100 }}>
         <RNButton title="test" color={"red"} />
       </View>
-      <Button title="test" />
+      <Button
+        my="sm"
+        p="sm"
+        shadowColor="black"
+        color="white"
+        textColor="black"
+        rounded="full"
+      >
+        Test
+      </Button>
+      <View style={{ flexDirection: "row" }}>
+        <Button
+          my="sm"
+          p="sm"
+          shadowColor="black"
+          color="white"
+          textColor="black"
+          rounded="xl"
+          width={100}
+        >
+          Test
+        </Button>
+
+        <Button
+          my="sm"
+          p="sm"
+          shadowColor="black"
+          color="white"
+          textColor="black"
+          rounded="xl"
+        >
+          Test
+        </Button>
+      </View>
+      <Button
+        my="sm"
+        shadowColor="black"
+        color="white"
+        textColor="black"
+        width={100}
+        height={100}
+        rounded="full"
+        shadowLength={3}
+      >
+        <Ionicons name="add" />
+      </Button>
+      <Card>
+        <Text>Test</Text>
+      </Card>
     </View>
   );
 }
@@ -20,6 +70,6 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10
+    margin: 10
   }
 });
