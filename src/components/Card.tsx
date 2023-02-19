5dbcbf5d22e5610fb8ea20424c33ab11a0e47302
Daft <daftdevs@gmail.com>
2023-02-19 21:36:11 +0700

    feat: add shadow, spacing, colors customization

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,136 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ViewStyle } from "react-native";
+import { useTheme } from "../provider/ThemeProvider";
+import { ColorsOptions, SpacingProps } from "../types";
 
-type CardProps = React.ComponentProps<typeof View> & {
-  children: React.ReactNode;
-};
+type CardProps = SpacingProps &
+  React.ComponentProps<typeof View> & {
+    children: React.ReactNode;
+    backgroundColor?: ColorsOptions;
+    style?: ViewStyle;
+    shadowPosition?: "bottom-right" | "bottom-left" | "top-right" | "top-left";
+    shadowLength?: number;
+    shadowStyle?: ViewStyle;
+    shadowColor?: ColorsOptions;
+  };
 
-const Card = ({ children }: CardProps) => {
-  return <View>{children}</View>;
+const Card = ({
+  p = "none",
+  px = "none",
+  py = "none",
+  pt = "none",
+  pr = "none",
+  pb = "none",
+  pl = "none",
+  m = "none",
+  mx = "none",
+  my = "none",
+  mt = "none",
+  mr = "none",
+  mb = "none",
+  ml = "none",
+  backgroundColor = "white",
+  shadowPosition = "bottom-right",
+  shadowLength = 6,
+  shadowStyle,
+  shadowColor = "black",
+  children,
+  style
+}: CardProps) => {
+  const { spacing, colors } = useTheme();
+
+  const shadowPositionMap = {
+    "bottom-right": {
+      top: shadowLength,
+      bottom: shadowLength * -1,
+      right: shadowLength * -1,
+      left: shadowLength
+    },
+    "bottom-left": {
+      top: shadowLength,
+      bottom: shadowLength * -1,
+      right: shadowLength,
+      left: shadowLength * -1
+    },
+    "top-right": {
+      top: shadowLength * -1,
+      bottom: shadowLength,
+      right: shadowLength * -1,
+      left: shadowLength
+    },
+    "top-left": {
+      top: shadowLength * -1,
+      bottom: shadowLength,
+      right: shadowLength,
+      left: shadowLength * -1
+    }
+  };
+
+  return (
+    <View
+      style={{
+        position: "relative",
+        flex: 1
+      }}
+    >
+      <View
+        style={[
+          {
+            padding: spacing[p],
+            paddingTop: spacing[pt],
+            paddingRight: spacing[pr],
+            paddingBottom: spacing[pb],
+            paddingLeft: spacing[pl],
+            paddingHorizontal: spacing[px],
+            paddingVertical: spacing[py],
+            margin: spacing[m],
+            marginHorizontal: spacing[mx],
+            marginVertical: spacing[my],
+            marginTop: spacing[mt],
+            marginRight: spacing[mr],
+            marginBottom: spacing[mb],
+            marginLeft: spacing[ml]
+          },
+          {
+            flex: 1,
+            backgroundColor: colors[backgroundColor],
+            borderWidth: 1,
+            borderColor: colors[shadowColor]
+          },
+          style
+        ]}
+      >
+        {children}
+      </View>
+      <View
+        style={[
+          {
+            padding: spacing[p],
+            paddingTop: spacing[pt],
+            paddingRight: spacing[pr],
+            paddingBottom: spacing[pb],
+            paddingLeft: spacing[pl],
+            paddingHorizontal: spacing[px],
+            paddingVertical: spacing[py],
+            margin: spacing[m],
+            marginHorizontal: spacing[mx],
+            marginVertical: spacing[my],
+            marginTop: spacing[mt],
+            marginRight: spacing[mr],
+            marginBottom: spacing[mb],
+            marginLeft: spacing[ml]
+          },
+          {
+            ...shadowPositionMap[shadowPosition],
+            position: "absolute",
+            backgroundColor: colors[shadowColor],
+            zIndex: -10
+          },
+          { ...shadowStyle }
+        ]}
+      />
+    </View>
+  );
 };
 
 export default Card;
