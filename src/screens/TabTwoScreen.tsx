a6201479c756108b025e0e3af65460e10bed2b39
Daft <daftdevs@gmail.com>
2023-02-26 21:43:32 +0700

    ref: preview card component

--- src/screens/TabTwoScreen.tsx
+++ src/screens/TabTwoScreen.tsx
@@ -1,68 +1,72 @@
 import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import { Button as RNButton, StyleSheet, View } from "react-native";
+import { Button as RNButton, StyleSheet, View, ScrollView } from "react-native";
 import Button from "../components/Button";
 import Card from "../components/Card";
+import Input from "../components/Input";
 import Text from "../components/Text";
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text variants="h4" mb="sm">
-        Button
-      </Text>
-      <View style={{ width: 100 }}>
-        <RNButton title="test" color={"red"} />
-      </View>
-      <Button
-        my="sm"
-        p="sm"
-        shadowColor="black"
-        color="white"
-        textColor="black"
-        rounded="full"
-      >
-        Test
-      </Button>
-      <View style={{ flexDirection: "row" }}>
+      <ScrollView>
+        <Text variants="h4" mb="sm">
+          Button
+        </Text>
+        <View style={{ width: 100 }}>
+          <RNButton title="test" color={"red"} />
+        </View>
         <Button
           my="sm"
           p="sm"
           shadowColor="black"
           color="white"
           textColor="black"
-          rounded="xl"
-          width={100}
+          rounded="full"
         >
           Test
         </Button>
+        <View style={{ flexDirection: "row" }}>
+          <Button
+            my="sm"
+            p="sm"
+            shadowColor="black"
+            color="white"
+            textColor="black"
+            rounded="xl"
+            width={100}
+          >
+            Test
+          </Button>
 
+          <Button
+            my="sm"
+            p="sm"
+            shadowColor="black"
+            color="white"
+            textColor="black"
+            rounded="xl"
+          >
+            Test
+          </Button>
+        </View>
         <Button
           my="sm"
-          p="sm"
           shadowColor="black"
           color="white"
           textColor="black"
-          rounded="xl"
+          width={100}
+          height={100}
+          rounded="full"
+          shadowLength={3}
         >
-          Test
+          <Ionicons name="add" />
         </Button>
-      </View>
-      <Button
-        my="sm"
-        shadowColor="black"
-        color="white"
-        textColor="black"
-        width={100}
-        height={100}
-        rounded="full"
-        shadowLength={3}
-      >
-        <Ionicons name="add" />
-      </Button>
-      <Card>
-        <Text>Test</Text>
-      </Card>
+        <Card p="sm" height={100}>
+          <Text>Test</Text>
+        </Card>
+        <Input mt="sm" />
+      </ScrollView>
     </View>
   );
 }
